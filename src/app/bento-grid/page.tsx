753102a8ea8c0a923f83b687a65c597f33f7088d
ae_jay <ajlimramirez@gmail.com>
2024-10-01 14:30:42 +0800

    added more blocks

--- src/app/bento-grid/page.tsx
+++ src/app/bento-grid/page.tsx
@@ -2,9 +2,11 @@ import { dm_sans } from "../fonts/fonts";
 import Image from "next/image";
 
 import FiveStars from "@/assets/bento-grid/illustration-five-stars.webp";
-import MultiPlatform from "@/assets/bento-grid/illustration-multiple-platforms.webp";
+import Calendar from "@/assets/bento-grid/illustration-consistent-schedule.webp";
+import XLogo from "@/assets/bento-grid/x-logo.svg";
+import InstaLogo from "@/assets/bento-grid/insta-logo.svg";
 
-const PURPLE_100 = "hsl(254,88%,90%)";
+// const PURPLE_100 = "hsl(254,88%,90%)";
 const PURPLE_500 = "hsl(256,67%,59%)";
 const YELLOW_100 = "hsl(31,66%,93%)";
 const YELLOW_500 = "hsl(39,100%,71%)";
@@ -15,7 +17,7 @@ export default function BentoGrid() {
     return (
         <div
             style={{ backgroundColor: WHITE }}
-            className={`py-8 px-4 h-screen w-screen ${dm_sans.className}`}
+            className={`py-8 px-4 w-screen ${dm_sans.className}`}
         >
             <div
                 className="rounded-[10px] flex flex-col items-center p-8 mb-8"
@@ -39,18 +41,82 @@ export default function BentoGrid() {
                     Over 4,000 5-star reviews
                 </div>
             </div>
-            <div className="rounded-[10px] drop-shadow-md bg-white">
-                <Image
-                    className="mx-4 w-auto py-4"
-                    src={MultiPlatform}
-                    alt="multiple platform image"
-                />
-
+            <div className="p-4 rounded-[10px] drop-shadow-md bg-white mb-8">
+                <div className="flex justify-between mb-4">
+                    <div className="flex drop-shadow-md bg-white rounded-full justify-center p-4 pr-6">
+                        <div className="flex items-center gap-2">
+                            <div
+                                className="rounded-full p-0.5"
+                                style={{ backgroundColor: YELLOW_500 }}
+                            >
+                                <Image
+                                    className="w-8 h-8"
+                                    src={InstaLogo}
+                                    alt="instagram logo"
+                                />
+                            </div>
+                            <div>
+                                <div
+                                    className="font-semibold -tracking-3"
+                                    style={{ color: BLACK }}
+                                >
+                                    @YourCo
+                                </div>
+                                <div className="text-black text-xs -mt-0.5 -tracking-2 opacity-60">
+                                    12k Followers
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex drop-shadow-md bg-white rounded-full p-4 pr-6">
+                        <div className="flex items-center gap-2">
+                            <div
+                                className="rounded-full w-9 h-9 p-1.5"
+                                style={{ backgroundColor: YELLOW_500 }}
+                            >
+                                <Image
+                                    className=""
+                                    src={XLogo}
+                                    alt="instagram logo"
+                                />
+                            </div>
+                            <div>
+                                <div
+                                    className="font-semibold -tracking-3"
+                                    style={{ color: BLACK }}
+                                >
+                                    @YourCo
+                                </div>
+                                <div className="text-black text-xs -mt-0.5 -tracking-2 opacity-60">
+                                    8k Followers
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div
-                    className="font-medium text-2xl -tracking-3 leading-[22px] px-4 pb-4"
+                    className="font-medium text-2xl -tracking-3 leading-[22px]"
                     style={{ color: BLACK }}
                 >
                     Manage multiple accounts and platforms.
+                </div>
+            </div>
+            <div
+                className="rounded-[10px]"
+                style={{ backgroundColor: YELLOW_500 }}
+            >
+                <div
+                    className="p-4 pr-8 font-medium text-2xl -tracking-3 leading-[22px]"
+                    style={{ color: BLACK }}
+                >
+                    Maintain a consistent posting schedule.
+                </div>
+                <div className="pl-4 ">
+                    <Image
+                        className="w-[195px] h-[128px] object-top object-cover"
+                        src={Calendar}
+                        alt="calendar"
+                    />
                 </div>
             </div>
         </div>
